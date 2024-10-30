a3c94e18a45a24c763c3b194431e061da29e578d
ansetechnoapp <anselmetechnoapp@gmail.com>
2024-10-30 12:29:24 +0100

    edit 12:29 PM

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -6,11 +6,3 @@ export const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_PUBLIC_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_PUBLIC_APP_ID,
 };
-export const firebaseConfigRemix = {
-  apiKey: process.env.VITE_PUBLIC_API_KEY,
-  authDomain: process.env.VITE_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.VITE_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.VITE_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_PUBLIC_APP_ID,
-};
